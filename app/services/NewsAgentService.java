434adde7e0bb6fbe2032ea8bc1847238f1f257e8
Amitvik <amitvikram299@gmail.com>
2018-04-14 06:05:46 +0000

    Added newsAgentservice

--- app/services/NewsAgentService.java
+++ app/services/NewsAgentService.java
@@ -11,5 +11,28 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class NewsAgentService {
+    public NewsAgentResponse getNewsAgentResponse(String query, UUID sessionid) {
+        NewsAgentResponse newsAgentResponse = new NewsAgentResponse();
+        try {
+            WSRequest queryRequest = WS.url("https://api.api.ai/api/query");
+            CompletionStage<WSResponse> responsePromise = queryRequest
+                    .setQueryParameter("v", "20150910")
+                    .setQueryParameter("query", query)
+                    .setQueryParameter("lang", "en")
+                    .setQueryParameter("sessionId", sessionid.toString())
+                    .setQueryParameter("timezone", "2017-08-09T03:25:23+0530")
+                    .setHeader("Authorization", "Bearer 054a388ef08e46c3beb61cd9a12dd13f")
+                    .get();
+            JsonNode response = responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
+            newsAgentResponse.query = response.get("result").get("parameters").get("keyword").asText().isEmpty() ?
+                    (response.get("result").get("parameters").get("source").asText().isEmpty()
+                            ? response.get("result").get("parameters").get("category").asText()
+                            : response.get("result").get("parameters").get("source").asText() )
+                    : response.get("result").get("parameters").get("keyword").asText() ;
 
+        }catch (Exception e){
+            e.printStackTrace();
+    }return newsAgentResponse;
 }
+}
+
