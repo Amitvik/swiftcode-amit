e3a30b7ef0012979b5148d69d890c0b3c9c119bd
Amitvik <amitvikram299@gmail.com>
2018-04-14 11:49:18 +0000

    day2

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,23 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'Hi what up'
+	}
+        {
+            'sender': 'USER',
+            'text': 'what is the time'
+	}
+        {
+            'sender': 'BOT',
+            'text': 'it is half past noon'
+	}]
 
 
 });
